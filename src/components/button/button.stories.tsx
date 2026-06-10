ddf6a95e28bf2aa25691790a21f7a88ebcda292f
slauyama <7174954+slauyama@users.noreply.github.com>
2026-06-09 20:29:19 -0700

    Add wrap to button story

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -20,7 +20,7 @@ type Story = StoryObj<typeof Button>;
 
 export const AllSurfaces: Story = {
   render: () => (
-    <div className="flex flex-row gap-2">
+    <div className="flex flex-row flex-wrap gap-2">
       {ALL_SURFACES.map((surface) => (
         <Button key={surface} surface={surface}>
           {surface} Button
